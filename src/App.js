292c1c009bb41765d7d287caa6361bb015d8eb19
Dima <webbootcamp2020@gmail.com>
2021-05-05 23:09:01 +0300

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import * as ROUTES from './constants/routes'
-import { Home, Browse, Signin, Signup } from './pages/'
-import { IsUserRedirect } from './helpers/routes'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signin, Signup } from "./pages/";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks/";
 
 function App() {
-  const user = {};
-  return (
-    <Router>
-        <IsUserRedirect exact user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
-            <Signin />
-        </IsUserRedirect>
-    	<IsUserRedirect exact user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
-            <Signup />
-        </IsUserRedirect>
-    	<Route exact path={ROUTES.BROWSE} >
-    		<Browse />
-    	</Route>
-    	<Route exact path={ROUTES.HOME}>
-    		<Home />
-    	</Route>
-    </Router>
-  );
+    const user = useAuthListener();
+    console.log(user);
+    return (
+        <Router>
+            <IsUserRedirect
+                exact
+                user={user}
+                loggedInPath={ROUTES.BROWSE}
+                path={ROUTES.SIGN_IN}
+            >
+                <Signin />
+            </IsUserRedirect>
+            <IsUserRedirect
+                exact
+                user={user}
+                loggedInPath={ROUTES.BROWSE}
+                path={ROUTES.SIGN_UP}
+            >
+                <Signup />
+            </IsUserRedirect>
+            <ProtectedRoute user={user} exact path={ROUTES.BROWSE}>
+                <Browse />
+            </ProtectedRoute>
+
+            <IsUserRedirect
+                user={user}
+                loggedInPath={ROUTES.BROWSE}
+                exact
+                path={ROUTES.HOME}
+            >
+                <Home />
+            </IsUserRedirect>
+        </Router>
+    );
 }
 
 export default App;
